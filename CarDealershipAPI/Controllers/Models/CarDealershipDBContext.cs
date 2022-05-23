28c9cb95f8e03bfdba2b9c6946469ab345f99777
sadams31 <sadams@wwiflint.com>
2022-05-23 11:15:11 -0400

    NOW I fixed the connection

--- CarDealershipAPI/Controllers/Models/CarDealershipDBContext.cs
+++ CarDealershipAPI/Controllers/Models/CarDealershipDBContext.cs
@@ -24,7 +24,7 @@ namespace GC_CAR_DEALERSHIP.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=WWP-SADAMS-LPT\\SQLEXPRESS;Database=GCCarDealershipDB;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=WWP-SADAMS-LPT\\SQLEXPRESS;Database=CarDealership;Trusted_Connection=True;");
             }
         }
 
